fce9ef76678a0817e266635855af1f12eb7fc42c
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-10 23:12:07 +0200

    Rename Watches and FireWatches to Matches and FireMatches

---
+++ Assets/Scripts/Game/Actors/Player_Watches/FireMatches.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FireMatches : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Matches playerWatches = collision.gameObject.GetComponent<Matches>();
+        if (playerWatches != null && !playerWatches.OnFire)
+        {
+            playerWatches.Alight();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Game/Actors/Player_Watches/Matches.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Matches : MonoBehaviour {
+
+    // In editor for debug
+    [SerializeField] private bool m_OnFire = true; 
+    [SerializeField] private GameObject m_Gfx;
+    [SerializeField] private GameObject m_Fire;
+
+    [Header("To debug")]
+    [SerializeField] private Material m_materialOnFire;
+
+    public bool OnFire
+    {
+        get
+        {
+            return m_OnFire;
+        }
+    }
+
+    private void Start () {
+        CheckIsOnFire();
+    }
+
+    private void CheckIsOnFire()
+    {
+        if (m_OnFire)
+        {
+            SetMaterialOnFire();
+            InstantiateFire();
+        }
+    }
+
+    private void InstantiateFire()
+    {
+        GameObject fire = Instantiate(m_Fire);
+        fire.transform.parent = transform;
+        fire.transform.localPosition = Vector2.zero;
+    }
+
+    private void SetMaterialOnFire()
+    {
+        m_Gfx.GetComponent<MeshRenderer>().material = m_materialOnFire;
+    }
+
+    public void Alight()
+    {
+        m_OnFire = true;
+        CheckIsOnFire();
+    }
+}
