2a5461f833118c4bcfe84a81799bad3056866b69
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-04-11 12:40:01 +0200

    Added: Player2 Firefighter's lance.

--- Assets/Scripts/Game/Actors/Water/Water.cs
+++ Assets/Scripts/Game/Actors/Water/Water.cs
@@ -8,23 +8,32 @@ namespace Assets.Scripts.Game.Actors.Water {
     public class Water : MonoBehaviour {
 
         [SerializeField]
+        private float m_Z_INDEX_SNAP = -0.5f;
+        [SerializeField]
+        private Transform m_ground;
+        [SerializeField]
         private float reduceSpeed = 0.003f; // change to lifeTime ?
+        private Rigidbody rb;
+        
 
         protected void Start () {
+            rb = gameObject.GetComponent<Rigidbody>();
+            m_ground = GameObject.Find("Level").transform;
         }
 
         protected void Update () {
             Vector3 lScale = gameObject.transform.localScale;
 
-            lScale.z = Mathf.Max(0, lScale.z - reduceSpeed);
+            lScale.y = Mathf.Max(0, lScale.y - reduceSpeed);
             lScale.x = Mathf.Max(0, lScale.x - reduceSpeed);
 
-            if (lScale.z == 0) {
+            if (lScale.y == 0) {
                 DestroyImmediate(this);
                 return;
             }
 
             gameObject.transform.localScale = lScale;
+            CheckCollisionGround();
         }
 
         private void OnTriggerEnter (Collider collision) {
@@ -32,6 +41,17 @@ namespace Assets.Scripts.Game.Actors.Water {
 
             if (burnable)
                 burnable.TryExtinguish();
+        }
+
+        private void CheckCollisionGround() {
+            if (transform.position.z >= m_ground.position.z) {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y,
+                    m_Z_INDEX_SNAP
+                );
+                rb.isKinematic = true;
+            }
         }
 
 
