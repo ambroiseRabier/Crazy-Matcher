6af9fe3a71d0d0e21481036bce81373afe67c7b3
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-12 12:11:47 +0200

    Winscreen update

--- Assets/Scripts/Game/UI/WinScreen.cs
+++ Assets/Scripts/Game/UI/WinScreen.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class WinScreen : CanvasScreen {
     [SerializeField] private GameObject m_mamyPanel;
     [SerializeField] private GameObject m_matchesPanel;
+
+    public void SetWinnerTeam(GameManager.Team team)
+    {
+        m_mamyPanel.SetActive(team == GameManager.Team.FIRE_FIGHTER);
+        m_matchesPanel.SetActive(team == GameManager.Team.MATCHES);
+    }
 }
