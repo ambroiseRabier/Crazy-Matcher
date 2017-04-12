d5e67f98e2e1c8084a2fc1440aceaad8988a4459
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-13 00:38:23 +0200

    Add VSIntroduction screen

---
+++ Assets/VSIntroductionScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VSIntroductionScreen : CanvasScreen<VSIntroductionScreen> {
+    [SerializeField] private Sprite m_mamySprite;
+    [SerializeField] private Sprite m_matchesSprite;
+
+    [SerializeField] private Image m_p1Image;
+    [SerializeField] private Image m_p2Image;
+
+    public void SetP1Team(GameManager.Team p1Team)
+    {
+        SetTeamSpriteOnImage(p1Team, m_p1Image);
+    }
+
+    public void SetP2Team(GameManager.Team p2Team)
+    {
+        SetTeamSpriteOnImage(p2Team, m_p2Image);
+    }
+
+    private void SetTeamSpriteOnImage(GameManager.Team team, Image image)
+    {
+        image.sprite = (team == GameManager.Team.MATCHES) ? m_matchesSprite : m_mamySprite;
+    }
+}
