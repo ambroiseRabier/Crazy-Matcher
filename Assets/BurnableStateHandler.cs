3495849e039c8b3cc88fb98b4052c67a7f6bfd8b
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-12 18:35:44 +0200

    Add BurnStateHandler

---
+++ Assets/BurnableStateHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableStateHandler : MonoBehaviour {
+    [SerializeField] private List<GameObject> m_states = new List<GameObject>();
+    [SerializeField] private Burnable burnable;
+
+	private void Awake () {
+        burnable.OnBurnRatioProgress += Burnable_OnBurnRatioProgress;
+        EnableStateFromBurnRatio(burnable.BurnRatio);
+    }
+
+    private void Burnable_OnBurnRatioProgress(Burnable burnable, float newBurnRatio)
+    {
+        EnableStateFromBurnRatio(newBurnRatio);
+    }
+
+    private void EnableStateFromBurnRatio(float burnRatio)
+    {
+        EnableStateFromIndex(Mathf.RoundToInt(burnRatio * (m_states.Count - 1)));
+    }
+
+    private void EnableStateFromIndex(int index)
+    {
+        for (int i = m_states.Count-1; i > -1; i--)
+            m_states[i].SetActive(index == i);
+    }
+
+    private void OnDestroy()
+    {
+        burnable.OnBurnRatioProgress -= Burnable_OnBurnRatioProgress;
+    }
+}
