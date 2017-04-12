c88123978e3a5e6c66c5c41d498bbbd97420879f
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-12 13:04:34 +0200

    Update HUD, add gauge

--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : CanvasScreen<HUD>
 {
     [SerializeField] private UIPlayerScore m_p1ScoreComp;
     [SerializeField] private UIPlayerScore m_p2ScoreComp;
+    [SerializeField] private Slider m_waterTankSlider;
 
     public UIPlayerScore P1ScoreComp { get { return m_p1ScoreComp; } }
     public UIPlayerScore P2ScoreComp { get { return m_p2ScoreComp; } }
+    public Slider WaterTankSlider { get { return m_waterTankSlider; } }
 }
