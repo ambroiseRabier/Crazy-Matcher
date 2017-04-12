9d551fd9f420f6cf64c8750d3a32977f971868b6
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-12 19:10:49 +0200

    Head in fire

---
+++ Assets/TranslateFromBurnRatio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TranslateFromBurnRatio : MonoBehaviour {
+    [SerializeField] private Burnable m_burnable;
+    [SerializeField] private Transform m_targetedTransform;
+    private Vector3 m_localPositionOnStart;
+	
+	private void Awake () {
+        m_localPositionOnStart = transform.localPosition;
+        m_burnable.OnBurnRatioProgress += Burnable_OnBurnRatioProgress;
+	}
+
+    private void Burnable_OnBurnRatioProgress(Burnable burnable, float newBurnRatio)
+    {
+        transform.localPosition = Vector3.Lerp(m_localPositionOnStart, m_targetedTransform.localPosition, newBurnRatio);
+    }
+
+    private void OnDestroy () {
+        m_burnable.OnBurnRatioProgress -= Burnable_OnBurnRatioProgress;
+    }
+}
