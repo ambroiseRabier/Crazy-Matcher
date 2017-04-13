e1b0e67824d682908ce8726c5b56514a542f7aca
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-04-13 23:00:52 +0200

    Added: clamp firefighter in viewPort.

---
+++ Assets/Scripts/Game/Actors/Player_Firefighter/ClampInViewPort.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game.Actors.Player_Firefighter {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ClampInViewPort : MonoBehaviour {
+
+        protected void Start() {
+
+        }
+
+        protected void Update() {
+            Vector3 pos = UnityEngine.Camera.main.WorldToViewportPoint(transform.position);
+            pos.x = Mathf.Clamp01(pos.x);
+            pos.y = Mathf.Clamp01(pos.y);
+            transform.position = UnityEngine.Camera.main.ViewportToWorldPoint(pos);
+        }
+    }
+}
