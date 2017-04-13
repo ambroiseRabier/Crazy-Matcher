23f98ba5d3368500b6559f210a947c5e0344c1bc
Théo Sabattié <t.sabattie@student.isartdigital.com>
2017-04-13 20:57:58 +0200

    prevent error

--- Assets/Scripts/Game/UI/TempScreen.cs
+++ Assets/Scripts/Game/UI/TempScreen.cs
@@ -2,6 +2,11 @@
 using UnityEngine;
 
 public class TempScreen<T> : CanvasScreen<T> where T : Component {
+    private void Start()
+    {
+        Close();
+    }
+
     public new void Open(Action action = null)
     {
         gameObject.SetActive(true);
