382431af2d9ed2ca0b781325ac7258844ad6605d
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-04-13 12:04:54 +0200

    Modified: player controller matche.

--- Assets/Scripts/Game/VelocityFromControllerMatche.cs
+++ Assets/Scripts/Game/VelocityFromControllerMatche.cs
@@ -10,24 +10,35 @@ namespace Assets.Scripts.Game {
 
         [SerializeField] private float m_turnSpeed = 0.1f; // 0 to 1;
         /*[SerializeField] */private float m_maxTurnAnglePerFrame = 15f;
+        /*[SerializeField] */private float m_maxTurnAnglePerFrameCar = 5f;
         //private Vector3 previousPosition = Vector3.zero; // don't use transform.rotation dummy, get the direction from last position
         private float previousInputAngle; // don't use transform.rotation dummy, get the direction from last position
         private Vector3 previousDirection = Vector3.zero;
         private bool firstInputSet = false;
 
+        private Burnable m_burnableComponent;
+
+        protected void Start () {
+            m_burnableComponent = gameObject.GetComponent<Burnable>();
+        }
+
         protected void Update () {
                 
         }
 
-        override protected void FixedUpdate () {
-            if (m_Controller && m_Controller.Joystick.normalized != Vector3.zero)
-            {
-                if (firstInputSet)
-                {
-                    ThirdTest();
-                }
-                else
-                {
+        override protected void FixedUpdate() {
+
+            if (m_Controller && m_Controller.Joystick.normalized != Vector3.zero) {
+                //SixthTest();
+                if (firstInputSet) {
+
+
+                    //ThirdTest();
+                    //FourthTest(); // choose one
+                    SeventhController(); // choosen one whit "speed growing whit death"
+                    //FirstTest();
+                    //FifthTest();
+                } else {
                     firstInputSet = true;
                     //previousInputAngle = Mathf.Atan2(m_Controller.Joystick.normalized.y, m_Controller.Joystick.normalized.x);
                     previousDirection = m_Controller.Joystick.normalized;
@@ -35,14 +46,44 @@ namespace Assets.Scripts.Game {
                 }
 
             }
-
         }
 
+        void SeventhController() {
+            print(m_burnableComponent.BurnRatio);
+            //print(m_burnableComponent.Bu);
+            m_Rigidbody.velocity = m_Controller.Joystick.normalized * m_Speed ;//* m_burnableComponent.BurnRatio);
+        }
+
+        //physic control, whit max speed
+        void SixthTest () {
+            m_Rigidbody.AddForce(previousDirection * m_Speed);
+            m_Rigidbody.velocity = Vector3.Lerp(m_Rigidbody.velocity, previousDirection * m_Speed, m_turnSpeed);
+            if (m_Controller.Joystick.normalized != Vector3.zero)
+                previousDirection = m_Controller.Joystick.normalized;
+        }
+
+        //car control
+        void FifthTest () {
+            float angleDiff = m_Controller.Joystick.normalized.x * m_maxTurnAnglePerFrameCar; // avec vitesse 200, mais collision galère
+            Vector3 newDirection = Quaternion.Euler(0, 0, angleDiff) * previousDirection;
+            float newAngleDiff = Vector3.Angle(newDirection, m_Controller.Joystick.normalized);
+
+            m_Rigidbody.velocity = newDirection.normalized * m_Speed;
+            previousDirection = newDirection.normalized;
+        }
+
+        //normal control
+        void FourthTest () {
+            m_Rigidbody.velocity = m_Controller.Joystick.normalized * m_Speed;
+        }
+
+        // physic control, no alway max speed
         void ThirdTest() {
             m_Rigidbody.AddForce(m_Controller.Joystick.normalized * m_Speed);
             m_Rigidbody.velocity = Vector3.Lerp(m_Rigidbody.velocity, m_Controller.Joystick.normalized * m_Speed, m_turnSpeed);
         }
 
+        // limited angle turn
         void FirstTest () {
             //Vector3 newDirection = Vector3.zero;
             //float maxTurnPerFrameRad = m_maxTurnAnglePerFrame * Mathf.PI / 180;
@@ -70,6 +111,7 @@ namespace Assets.Scripts.Game {
         float m_maxAngleRotation = 15f;   // Défini comme membre sérializé
         Vector3 m_currentDirection; // Actuelle direction, défini comme membre
 
+        // not working
         void SecondTest () {
 
             float currentRadian = Mathf.Atan2(m_currentDirection.y, m_currentDirection.x);
