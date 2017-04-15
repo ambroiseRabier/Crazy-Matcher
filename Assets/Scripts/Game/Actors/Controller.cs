4e93a133e17e061f3eb365ed74c09c46b227ff7a
ambroiseRabier <rabier.ambroise@outlook.fr>
2017-04-15 15:00:31 +0200

    Added: Rewired on controller.

--- Assets/Scripts/Game/Actors/Controller.cs
+++ Assets/Scripts/Game/Actors/Controller.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Game.Actors;
+using UnityEngine;
 
 public class Controller : MonoBehaviour {
+    [Tooltip("Rewired override options bellow.")]
+    [SerializeField] private bool m_useRewired;
+    [Header("Whitout Rewired")]
     [SerializeField] private string m_HorizontalInputName;
     [SerializeField] private string m_VerticalInputName;
     [SerializeField] private string m_FireInputName;
@@ -12,15 +16,34 @@ public class Controller : MonoBehaviour {
     public bool Fire { get; private set; }
     public float m_triggerLeft { get; private set; }
     public float m_triggerRight { get; private set; }
+    private ControllerRewired rewiredController;
+
+    private void Start () {
+        rewiredController = gameObject.GetComponent<ControllerRewired>();
+    }
 
     private void Update()
     {
-        m_Joystick.x    = Input.GetAxis(m_HorizontalInputName);
-        m_Joystick.y    = Input.GetAxis(m_VerticalInputName);
-        Fire            = Input.GetButton(m_FireInputName);
-        
-        m_triggerLeft   = Input.GetAxis(m_triggerLeftName);
-        m_triggerRight  = Input.GetAxis(m_triggerRightName);
+        if (m_useRewired)
+            UseRewired();
+        else
+            UseDefaultInputManager();
+    }
 
+    private void UseRewired () {
+        m_Joystick = rewiredController.m_move;
+        Fire = rewiredController.m_fire;
+
+        m_triggerLeft = rewiredController.m_triggerLeft;
+        m_triggerRight = rewiredController.m_triggerRight;
+    }
+
+    private void UseDefaultInputManager () {
+        m_Joystick.x = Input.GetAxis(m_HorizontalInputName);
+        m_Joystick.y = Input.GetAxis(m_VerticalInputName);
+        Fire = Input.GetButton(m_FireInputName);
+
+        m_triggerLeft = Input.GetAxis(m_triggerLeftName);
+        m_triggerRight = Input.GetAxis(m_triggerRightName);
     }
 }
---
+++ Assets/Scripts/Game/Actors/ControllerRewired.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Rewired;
+
+namespace Assets.Scripts.Game.Actors {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ControllerRewired : MonoBehaviour {
+
+        [SerializeField] private int playerId;
+        public Vector2 m_move { get; private set; }
+        public bool m_fire { get; private set; }
+        public float m_triggerLeft { get; private set; }
+        public float m_triggerRight { get; private set; }
+        private Player m_player;
+
+        protected void Awake () {
+            m_player = ReInput.players.GetPlayer(playerId);
+            m_move = Vector2.zero;
+        }
+
+        protected void Start () {
+
+        }
+
+        protected void Update () {
+            GetInput();
+        }
+
+        private void GetInput () {
+            // get input by name or action id
+            m_move = new Vector2(
+                m_player.GetAxis("Move Horizontal"),
+                m_player.GetAxis("Move Vertical")
+            );           
+            m_fire = m_player.GetButton("Fire");
+            m_triggerLeft = m_player.GetAxis("Press Trigger Left");
+            m_triggerRight = m_player.GetAxis("Press Trigger Right");
+        }
+
+    }
+}
